109ab0a8ab1ec57d9c12ee6c3449d51df2b75dfd
Mynhardt Burger <mynhardt@gmail.com>
2023-03-05 21:44:40 -0500

    Neaten code

--- js/sketch.js
+++ js/sketch.js
@@ -1,48 +1,39 @@
+const PAD_WIDTH = 500;
+const PAD_HEIGHT = (PAD_WIDTH / 4) * 3;
+
 const inputRows = document.getElementById("rows");
 const inputColumns = document.getElementById("columns");
-const styleId = "pixelStyle";
-let pixelClass = document.getElementById(styleId);
+
+let pixelClass = document.createElement("style");
+pixelClass.id = "pixelStyle";
+document.body.appendChild(pixelClass);
 
 function createPixels(columns, rows) {
   const sketchpad = document.getElementById("sketchpad");
   sketchpad.style.gridTemplateColumns = `repeat(${columns}, auto)`;
   sketchpad.style.gridTemplaterows = `repeat(${rows}, auto)`;
 
-  const pixelWidth = `${500 / columns}px`;
-  const pixelheight = `${500 / rows}px`;
-  updatePixelStyle(500 / columns, 500 / rows);
-
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < columns; col++) {
-      pixel = sketchpad.appendChild(document.createElement("div"));
-      pixel.classList.add("pixel");
-      pixel.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "#161616";
-      });
-    }
+  for (let i = 0; i < rows * columns; i++) {
+    pixel = sketchpad.appendChild(document.createElement("div"));
+    pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", function () {
+      this.style.backgroundColor = "#161616";
+    });
   }
 }
 
 function updatePixelStyle(width, height) {
-  if (pixelClass === null) {
-    pixelClass = document.createElement("style");
-    pixelClass.id = styleId;
-    document.body.appendChild(pixelClass);
+  while (pixelClass.sheet.cssRules.length > 0) {
+    pixelClass.sheet.deleteRule(0);
   }
 
-  let style = pixelClass.sheet;
-
-  while (style.cssRules.length > 0) {
-    style.deleteRule(0);
-  }
-
-  style.insertRule(
+  pixelClass.sheet.insertRule(
     `.pixel {
     border: 1px solid #edecea;
     width: ${width}px;
     height: ${height}px;
   }`,
-    style.cssRules.length
+    pixelClass.sheet.cssRules.length
   );
 }
 
@@ -50,15 +41,20 @@ function updatePadSize(columns, rows) {
   let pixels = document.querySelectorAll(".pixel");
   pixels.forEach((x) => x.remove());
   createPixels(columns, rows);
+  updatePixelStyle(PAD_WIDTH / columns, PAD_HEIGHT / rows);
 }
 
 function addEventListeners() {
   const updateBtn = document.getElementById("updateBtn");
   updateBtn.addEventListener("click", () => {
-    updatePixelStyle(500 / inputColumns.value, 500 / inputRows.value);
+    updatePixelStyle(
+      PAD_WIDTH / inputColumns.value,
+      PAD_HEIGHT / inputRows.value
+    );
     updatePadSize(inputColumns.value, inputRows.value);
   });
 }
 
 createPixels(inputColumns.value, inputRows.value);
+updatePixelStyle(PAD_WIDTH / inputColumns.value, PAD_HEIGHT / inputRows.value);
 addEventListeners();
